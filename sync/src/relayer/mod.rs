bd3c58a688659578d22f6ff97e2789dc1098f870
Eval EXEC <execvy@gmail.com>
2023-05-16 09:42:00 +0800

    Refactor: move `header_map` to `ckb_shared`
    Signed-off-by: Eval EXEC <execvy@gmail.com>

--- sync/src/relayer/mod.rs
+++ sync/src/relayer/mod.rs
@@ -20,7 +20,7 @@ use self::get_block_transactions_process::GetBlockTransactionsProcess;
 use self::get_transactions_process::GetTransactionsProcess;
 use self::transaction_hashes_process::TransactionHashesProcess;
 use self::transactions_process::TransactionsProcess;
-use crate::types::{ActiveChain, BlockNumberAndHash, SyncShared};
+use crate::types::{ActiveChain, SyncShared};
 use crate::utils::{metric_ckb_message_bytes, send_message_to, MetricDirection};
 use crate::{Status, StatusCode};
 use ckb_chain::chain::ChainController;
@@ -31,6 +31,7 @@ use ckb_network::{
     SupportProtocols, TargetSession,
 };
 use ckb_shared::block_status::BlockStatus;
+use ckb_shared::types::BlockNumberAndHash;
 use ckb_systemtime::unix_time_as_millis;
 use ckb_tx_pool::service::TxVerificationResult;
 use ckb_types::{
