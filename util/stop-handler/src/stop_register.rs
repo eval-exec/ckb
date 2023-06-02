81ab91092614b5680188e96611c7df34761747b6
Eval EXEC <execvy@gmail.com>
2023-06-02 09:58:56 +0800

    add wait_all_ckb_services_exit

--- util/stop-handler/src/stop_register.rs
+++ util/stop-handler/src/stop_register.rs
@@ -1,37 +1,37 @@
 use ckb_util::Mutex;
 
-struct CkbHandle {
+struct CkbServiceHandles {
     thread_handles: Vec<std::thread::JoinHandle<()>>,
     tokio_handles: Vec<tokio::task::JoinHandle<()>>,
 }
 
-impl Drop for CkbHandle {
-    fn drop(&mut self) {
-        for handle in self.thread_handles.drain(..) {
-            match handle.join() {
-                Ok(_) => {}
-                Err(e) => {
-                    todo!("log error")
-                }
+pub fn wait_all_ckb_services_exit() {
+    let mut handles = CKB_HANDLES.lock();
+    for handle in handles.thread_handles.drain(..) {
+        match handle.join() {
+            Ok(_) => {}
+            Err(e) => {
+                todo!("log error")
             }
         }
-        for handle in self.tokio_handles.drain(..) {
-            match tokio::runtime::Handle::current().block_on(handle) {
-                Ok(_) => {}
-                Err(e) => {
-                    todo!("log error")
-                }
+    }
+    for handle in handles.tokio_handles.drain(..) {
+        match tokio::runtime::Handle::current().block_on(handle) {
+            Ok(_) => {}
+            Err(e) => {
+                todo!("log error")
             }
         }
     }
 }
 
-static CKB_HANDLES: once_cell::sync::Lazy<Mutex<CkbHandle>> = once_cell::sync::Lazy::new(|| {
-    Mutex::new(CkbHandle {
-        thread_handles: vec![],
-        tokio_handles: vec![],
-    })
-});
+static CKB_HANDLES: once_cell::sync::Lazy<Mutex<CkbServiceHandles>> =
+    once_cell::sync::Lazy::new(|| {
+        Mutex::new(CkbServiceHandles {
+            thread_handles: vec![],
+            tokio_handles: vec![],
+        })
+    });
 
 static TOKIO_EXIT: once_cell::sync::Lazy<(
     tokio::sync::watch::Sender<bool>,
