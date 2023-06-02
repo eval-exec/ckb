90918687a2c879354adc5b1525d8920868c96962
Eval EXEC <execvy@gmail.com>
2023-06-02 09:58:56 +0800

    add wait_all_ckb_services_exit

--- util/stop-handler/src/lib.rs
+++ util/stop-handler/src/lib.rs
@@ -2,3 +2,8 @@ mod stop_handler;
 mod stop_register;
 
 pub use stop_handler::{SignalSender, StopHandler, WATCH_INIT, WATCH_STOP};
+
+pub use stop_register::{
+    broadcast_exit_signals, new_crossbeam_exit_rx, new_tokio_exit_rx, register_thread,
+    register_tokio, wait_all_ckb_services_exit,
+};
